8f76fcf2541af0547fc5544d29c4db15e1062cc0
WhereIsMyRum <polcik.piotr@gmail.com>
2020-01-25 19:06:14 +0100

    Improved github service

--- app/services/github-service.js
+++ app/services/github-service.js
@@ -3,41 +3,57 @@ const axios = require('axios');
 const { processReadme } = require('../utils/processReadme');
 
 const githubGetProjectsRequest = async (lang) => {
-    let url;
+    let url, data;
     if (lang !== "en") {
         return require(`../translations/${lang}.json`);
     } else {
         url = 'https://api.github.com/users/WhereIsMyRum/repos';
     }
-    const res = await axios.get(url);
-    if (res.status != 200) {
-        throw res.status;
+    try {
+        const res = await axios.get(url);
+        data = await res.data;
+        data = data.map((repo) => {
+            return {
+                'id': repo.id,
+                'name': repo.name,
+                'description': repo.description
+            }
+        });
+    } catch {
+        throw 500;
     }
-    let data = await res.data;
-    data = data.map((repo) => {
-        return {
-            'id': repo.id,
-            'name': repo.name,
-            'description': repo.description
-        }
-    });
     return data;
 }
 
 const githubGetProjectDetials = async (name, lang) => {
-    let url;
-    if (lang !== "en") {
-        url = `https://api.github.com/repos/WhereIsMyRum/${name}/contents/README_${lang}.md`;
+    const url_base = 'https://api.github.com/repos/WhereIsMyRum';
+    let url, res, parsedReadme;
+
+    if (lang !== 'en') {
+        url = url_base + `/${name}/contents/README_${lang}.md`;
     } else {
-        url = `https://api.github.com/repos/WhereIsMyRum/${name}/readme`;
+        url = url_base + `/${name}/readme`;
     }
-    const res = await axios.get(url)
-    if (res.status != 200) {
-        throw res.status;
+
+    try {
+        res = await axios.get(url)
+    } catch {
+        try {
+            url = url_base + `/${name}/readme`;
+            res = await axios.get(url);
+        } catch {
+            throw 500;
+        }
     }
-    const data = await res.data;
-    const parsedReadme = await processReadme(data['content']);
-    parsedReadme.title['url'] = data._links.html.match(/(https:\/\/github\.com\/WhereIsMyRum\/[0-9a-zA-Z-_]*\/)/g)[0];
+
+    try {
+        const data = await res.data;
+        parsedReadme = await processReadme(data['content']);
+        parsedReadme.title['url'] = data._links.html.match(/(https:\/\/github\.com\/WhereIsMyRum\/[0-9a-zA-Z-_]*\/)/g)[0];
+    } catch {
+        throw 500;
+    }
+
     return parsedReadme;
 }
 
