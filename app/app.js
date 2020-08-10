39fa7953711f231228980b4f57bdf4d0582b1fb4
WhereIsMyRum <polcik.piotr@gmail.com>
2020-08-10 17:05:56 +0200

    Added web-scraper project

--- app/app.js
+++ app/app.js
@@ -1,16 +1,16 @@
-const express = require('express')
-const app = express()
-const PORT = 8080
+const express = require('express');
+const app = express();
+const PORT = 8080;
 const routes = require('./routes');
 const { redisLookup } = require('./middlewares');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 
 app.use(cookieParser());
-app.use(redisLookup)
+app.use(redisLookup);
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-app.use('/api', routes.router)
+app.use('/api', routes.router);
 
 
-app.listen(PORT)
+app.listen(PORT);
